e9abe7cd1de39f8a3b38040dcd0b0a55e07f71b2
Christian Heyn <chris.heyn.dd@gmail.com>
2018-09-11 15:09:08 +0200

    example R2, POS 286

--- index.r
+++ index.r
@@ -1,13 +1,34 @@
+# helper =======================================================================
+map.i <- function(fn, data) {
+    result <- c()
+    index <- 1
+    for (value in data){
+        result[index] <- fn(value, index)
+        index <- index + 1
+    }
+    return(result)
+}
+
+map <- function(fn, data) {
+    result <- c()
+    index <- 1
+    for (value in data){
+        result[index] <- fn(value)
+        index <- index + 1
+    }
+    return(result)
+}
+# ==============================================================================
+
 xs <- 0:6
 
 # BLAU
 fnOrigin <- function(x) return(x^2)
-
 # GRAU
-fnTry <- function (x) return((6 * x) - 5)
+fnTry <- function (x) { return((6 * x) - 5) }
 
-ys <- mapply(fnOrigin, xs)
-ys.fn <- mapply(fnTry, xs)
+ys <- map(fnOrigin, xs)
+ys.fn <- map(fnTry, xs)
 
 err <- function(data) {
     data.mean = mean(data)
@@ -16,22 +37,24 @@ err <- function(data) {
 }
 
 ys.err = err(ys)
-ys.err
 
-ys.fn.pre = sum(mapply(function(d) {
-    return(d)
-}, ys))
+ys.fn.pre = map.i(function(v, i) {
+    return(abs(ys[i] - v) ^ 2)
+}, ys.fn)
 
-ys.fn.err = err(ys.fn.pre)
-ys.fn.err
+ys.fn.err = sum(ys.fn.pre)
+
+R2 = 1 - (ys.fn.err / ys.err)
 
 plot(
     x=0,
     y=0,
-    xlim=c(0,7),
-    ylim=c(0,40),
-    type="n"
+    xlim=c(0,max(xs)),
+    ylim=c(0,max(ys)),
+    type="n",
   )
 
 lines(x= xs, y=ys, col="#0099ff", lwd=2, type="l")
 lines(x= xs, y=ys.fn, col="#cccccc", lwd=2, type="b")
+legend(0.5, max(ys), legend=c("Origin", "Tryed", paste("R2: ", round(R2, digits=4))),
+       col=c("#0099ff", "#cccccc", "#ffffff"), lty=1, lwd=2)
